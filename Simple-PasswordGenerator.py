e815496aec3c75fcb07e935dab8f1c113e000fac
Ekta Kapase <106029464+ekta-kapase@users.noreply.github.com>
2022-10-19 21:55:32 +0530

    Update Simple-PasswordGenerator.py

--- Simple-PasswordGenerator.py
+++ Simple-PasswordGenerator.py
@@ -6,9 +6,9 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-ip_letters= int(input("How many letters would you like in your password?\n")) 
-ip_symbols = int(input(f"How many symbols would you like?\n"))
-ip_numbers = int(input(f"How many numbers would you like?\n"))
+ip_letters = int(input("How many letters would you like in your password?\n")) 
+ip_symbols = int(input("How many symbols would you like?\n"))
+ip_numbers = int(input("How many numbers would you like?\n"))
 
 r_letters = random.sample(letters, ip_letters)
 
